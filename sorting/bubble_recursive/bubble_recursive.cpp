c680a65bb6e80ab57406df1eca7e80f9a1fe795e
karanshah0206 <karanshah0206@outlook.com>
2022-09-12 01:29:50 +1000

    Recursive Bubble Sort

---
+++ sorting/bubble_recursive/bubble_recursive.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+// Sort List Using Recursive Bubble Sort
+void recursiveBubbleSort(vector<int>& list, size_t size) {
+	// Base Case
+	if (size < 2)
+		return;
+
+	// Single Pass of Bubble Sort
+	bool swapped = false;
+	for (int i = 1; i < size; i++) {
+		if (list[i-1] > list[i]) {
+			int temp = list[i-1];
+			list[i-1] = list[i];
+			list[i] = temp;
+			swapped = true;
+		}
+	}
+
+	// Escape
+	if (!swapped)
+		return;
+
+	// Recursive Step
+	recursiveBubbleSort(list, size-1);
+}
+
+int main(int argc, char* argv[]) {
+	// Check Arguments
+	if (argc < 3) {
+		cout << "Invalid Arguments." << "Usage: bubble_recursive <input_file> <output_file>" << endl;
+		return 1; // Error: Invalid Arguments
+	}
+
+	ifstream input(argv[1]);
+
+	// Check Input File
+	if (!input.good()) {
+		cout << "Error Opening File: " << argv[1] << endl;
+		return 2; // Error: Bad Input File
+	}
+
+	// Get List From Input File
+	int num;
+	vector<int> list;
+	while (input >> num)
+		list.push_back(num);
+	input.close();
+
+	// Sort List Using Recursive Bubble Sort
+	auto startTimer = high_resolution_clock::now();
+	recursiveBubbleSort(list, list.size());
+	auto stopTimer = high_resolution_clock::now();
+	auto duration = duration_cast<nanoseconds>(stopTimer - startTimer);
+	cout << duration.count() << " nanoseconds" << endl;
+
+	// Put Sorted List In Output File
+	ofstream output(argv[2]);
+	if (list.size() > 0) {
+		output << list[0];
+		for (int i = 1; i < list.size(); i++)
+			output << " " << list[i];
+	}
+	output.close();
+
+	return 0; // Success
+}
+
