22e0e567551352546d57efc127c00f8b4625f6ef
Karan Shah <karanshah0206@outlook.com>
2025-04-26 19:09:16 +0530

    Collinear assignment: Define custom type for drawable line segments

---
+++ princeton_algs_1/collinear/LineSegment.java
@@ -0,0 +1,51 @@
+public class LineSegment {
+    private final Point p, q;
+
+    /**
+     * Initializes a new line segment.
+     *
+     * @param p one endpoint
+     * @param q the other endpoint
+     * @throws NullPointerException if either <tt>p</tt> or <tt>q</tt>
+     *                              is <tt>null</tt>
+     */
+    public LineSegment(Point p, Point q) {
+        if (p == null || q == null) {
+            throw new IllegalArgumentException("argument to LineSegment constructor is null");
+        }
+
+        if (p.equals(q)) {
+            throw new IllegalArgumentException("both arguments to LineSegment constructor are the same point: " + p);
+        }
+
+        this.p = p;
+        this.q = q;
+    }
+
+    /**
+     * Draws this line segment to standard draw.
+     */
+    public void draw() {
+        p.drawTo(q);
+    }
+
+    /**
+     * Returns a string representation of this line segment
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this line segment
+     */
+    public String toString() {
+        return p + " -> " + q;
+    }
+
+    /**
+     * Unimplemented function, throws an exception if called.
+     *
+     * @throws UnsupportedOperationException if called
+     */
+    public int hashCode() {
+        throw new UnsupportedOperationException("hashCode() is not supported");
+    }
+}
